f299a040fc26a3247d1252e986d0a080bb9269f1
Niels de Klerk <mail@nielsdeklerk.nl>
2019-08-06 15:36:06 +0200

    0.0.1

---
+++ drivers/candela/device.js
@@ -0,0 +1,89 @@
+"use strict";
+
+const Homey = require("homey");
+
+const COMMAND_ON = Buffer.from("4340010101f4", "hex");
+const COMMAND_OFF = Buffer.from("4340020101f4", "hex");
+const COMMAND_DIM_HEADER = "4342";
+const COMMAND_DIM_FOOTER = "0101f4";
+const SERVICE_UUID = "fe87";
+const SERVICE_COMMAND_UUID = "aa7d3f342d4f41e0807f52fbf8cf7443";
+
+class CandelaBle extends Homey.Device {
+  onInit() {
+    this.log("device init: ", this.getName(), "id:", this.getData().id);
+    this.connectedBLEdevice = undefined;
+    this.registerAtHomey();
+  }
+
+  async connectBleService() {
+    try {
+      const BLEdevice = await Homey.ManagerBLE.find(this.getData().id);
+
+      //Workaround for BUGs in ManagerBLE, (Cashing Bug, Connection Bug)
+      if (BLEdevice.__peripheral) {
+        BLEdevice.__peripheral = null;
+      }
+
+      this.log("Connecting to BLE device");
+      this.connectedBLEdevice = await BLEdevice.connect();
+      const BleService = await this.connectedBLEdevice.getService(SERVICE_UUID);
+
+      return Promise.resolve(BleService);
+    } catch (error) {
+      this.log(`error connecting because, ${error}`);
+      return Promise.resolve(false);
+    }
+  }
+
+  async sendCommand(command) {
+    const BleService = await this.connectBleService();
+
+    if (!BleService) {
+      return Promise.resolve(false);
+    }
+
+    this.log(`Sending command: ${command.toString("hex")} to ${SERVICE_COMMAND_UUID}...`);
+    await BleService.write(SERVICE_COMMAND_UUID, command).catch(error => {
+      this.log(`Error sending command: ${command.toString("hex")} because of: ${error}`);
+    });
+
+    await this.connectedBLEdevice.disconnect().catch(error => {
+      this.log(`Error disconnect(): ${error}`);
+    });
+
+    return Promise.resolve(true);
+  }
+
+  registerAtHomey() {
+    this.registerCapabilityListener("onoff", async value => {
+      this.log(`Power is set to: ${value}`);
+      if (value) {
+        await this.sendCommand(COMMAND_ON);
+      } else {
+        await this.sendCommand(COMMAND_OFF);
+      }
+      return Promise.resolve(true);
+    });
+
+    this.registerCapabilityListener("dim", async value => {
+      this.log(`Brightness is set to ${value}`);
+
+      const brightness = parseInt(100 * value);
+      let command = "";
+      if (brightness == 0) {
+        command = COMMAND_OFF;
+      } else {
+        const brightnessHex = ("0" + Number(brightness).toString(16)).slice(-2);
+        command = COMMAND_DIM_HEADER + brightnessHex + COMMAND_DIM_FOOTER;
+      }
+      let levelBuffer = Buffer.from(command, "hex");
+
+      await this.sendCommand(levelBuffer); //this.ds.dimLevel(value));
+
+      return Promise.resolve(true);
+    });
+  }
+}
+
+module.exports = CandelaBle;
---
+++ drivers/candela/driver.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const Homey = require("homey");
+const SERVICE_UUID = "fe87";
+const PRESENTATION_NAME = "Ambiance Lamp";
+
+class CandelaBleDriver extends Homey.Driver {
+  onInit() {
+    this.log("Initializing candela BLE driver.");
+  }
+
+  async discoverLights() {
+    this.log("Discovering Candela BLE devices.");
+    try {
+      const discoveredDevices = await Homey.ManagerBLE.discover([SERVICE_UUID]);
+      const mappedDiscoveredDevices = discoveredDevices.map(bleDevice => {
+        const device = {
+          name: `${PRESENTATION_NAME} ${bleDevice.address.substr(bleDevice.address.length - 5)}`,
+          data: { id: bleDevice.uuid }
+        };
+
+        return device;
+      });
+      return Promise.resolve(mappedDiscoveredDevices);
+    } catch (error) {
+      return Promise.reject(error);
+    }
+  }
+
+  onPairListDevices(data, callback) {
+    this.discoverLights()
+      .then(deviceList => {
+        callback(null, deviceList);
+      })
+      .catch(error => {
+        callback(error);
+      });
+  }
+}
+
+module.exports = CandelaBleDriver;
