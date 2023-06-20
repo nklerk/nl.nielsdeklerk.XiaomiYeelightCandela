9d4dee5dc27800dc57c52c151033726982f7e34f
Martijn Poppen <m.poppen@ziggo.nl>
2023-06-20 21:17:12 +0200

    FIX: SDK3 pairing

--- drivers/candela/driver.js
+++ drivers/candela/driver.js
@@ -13,28 +13,33 @@ class CandelaBleDriver extends Homey.Driver {
     this.log("Discovering Candela BLE devices.");
     try {
       const discoveredDevices = await this.homey.ble.discover([SERVICE_UUID]);
-      const mappedDiscoveredDevices = discoveredDevices.map(bleDevice => {
-        const device = {
-          name: `${PRESENTATION_NAME} ${bleDevice.address.substr(bleDevice.address.length - 5)}`,
-          data: { id: bleDevice.uuid }
-        };
+      const mappedDiscoveredDevices = discoveredDevices.map((bleDevice) => ({
+          name: `${PRESENTATION_NAME} ${bleDevice.address.substr(
+            bleDevice.address.length - 5
+          )}`,
+          data: { id: bleDevice.uuid },
+        }));
 
-        return device;
-      });
       return Promise.resolve(mappedDiscoveredDevices);
     } catch (error) {
       return Promise.reject(error);
     }
   }
 
-  onPairListDevices(data, callback) {
-    this.discoverLights()
-      .then(deviceList => {
-        callback(null, deviceList);
-      })
-      .catch(error => {
-        callback(error);
-      });
+  async onPair(session) {
+    session.setHandler("list_devices", async () => {
+      try {
+        const devices = await this.discoverLights();
+
+        if(devices.length) {
+            return devices;
+        }
+        
+        return [];
+      } catch (error) {
+        this.log(error);
+      }
+    });
   }
 }
 
